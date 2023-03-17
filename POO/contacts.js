2be8f7451a786aedbe9e8d3abeb904cad2f6d1b6
Gara Vilches <garavilches@gmail.com>
2023-03-17 10:30:03 +0000

    tercer commit

--- POO/contacts.js
+++ POO/contacts.js
@@ -2,7 +2,6 @@ class Contacts {
     constructor()
     {
       this.array = []
-      this.array.push(persona1)
     }
     printPersons(){
         console.log(this.array)
--- POO/contactsTest.js
+++ POO/contactsTest.js
@@ -1,4 +1,12 @@
 const Contacts = require('./contacts')
+const Persona = require('./person')
 
+let persona1 = new Persona(69, 1.62, 1986)
+let persona2 = new Persona(85, 1.70, 1992)
 let contacto = new Contacts()
+
+contacto.array.push(persona1)
+contacto.array.push(persona2)
+
+
 console.log(contacto.printPersons())
